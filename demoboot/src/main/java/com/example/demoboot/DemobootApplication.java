b39dc698a29154fbb0a7fdd7e2384b63b694ef62
siva100625 <sivasubramanian.v2023ece@sece.ac.ingamil.com>
2025-07-03 10:31:46 +0530

    springboot

---
+++ demoboot/src/main/java/com/example/demoboot/DemobootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobootApplication.class, args);
+	}
+
+
+}
