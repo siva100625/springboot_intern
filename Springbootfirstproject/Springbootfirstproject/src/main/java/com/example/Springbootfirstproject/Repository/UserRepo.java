aadf4d80445cbcfda23d3d8549029a7545fecc3c
siva100625 <153293464+siva100625@users.noreply.github.com>
2025-07-09 10:39:55 +0530

    Delete Springbootfirstproject directory

--- Springbootfirstproject/Springbootfirstproject/src/main/java/com/example/Springbootfirstproject/Repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Springbootfirstproject.Repository;
-
-import com.example.Springbootfirstproject.Models.UserDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<UserDetails,Integer> {
-}
